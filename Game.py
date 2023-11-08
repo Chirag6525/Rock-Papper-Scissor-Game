5008dad1adebec889e12b069feb3cb6c8599daf9
Chirag Kumar <chiragkumar77777@gmail.com>
2023-11-08 12:07:46 +0530

    ReadMe File

---
+++ Game.py
@@ -0,0 +1,59 @@
+#Rock, Paper, Scissor Game
+
+import random
+#sign a value to the Rock, paper and Scissor
+Rock = 1
+Paper = 2
+Scissor = 3
+L = [Rock, Paper, Scissor]
+UserPoint = 0 
+CompPoint = 0
+i = 1
+
+print("Play will Commenced Five Times/n")
+while(i < 6):#Play for five times
+    print("\n\n1.🪨\n2.📄\n3.✂️")
+    #take a input from user
+    User = int(input("Enter Your Choice(1/2/3): "))
+    #computer generate its choice
+    Computer = random.choice(L)
+    print("Computer: {}".format(Computer))
+    if User == 1:
+        if Computer == 1:
+            print("Its a Tie!!!")
+            UserPoint += 1
+            CompPoint += 1
+        elif Computer == 2:
+            print("Computer won!!!")
+            CompPoint += 1
+        else:
+            print("User won!!!")
+            UserPoint += 1
+    elif User == 2:
+        if Computer == 2:
+            print("Its a Tie!!!")
+            UserPoint += 1
+            CompPoint += 1
+        elif Computer == 3:
+            print("Computer won!!!")
+            CompPoint += 1
+        else:
+            print("User won!!!")
+            UserPoint += 1
+    elif User == 3:
+        if Computer == 3:
+            print("Its a Tie!!!")
+            UserPoint += 1
+            CompPoint += 1
+        elif Computer == 1:
+            print("Computer won!!!")
+            CompPoint += 1
+        else:
+            print("User won!!!")
+            UserPoint += 1
+    i += 1
+print("UserPoint : {}\nComputerPoint: {}".format(UserPoint, CompPoint))
+if UserPoint > CompPoint:
+    print("The Winner is User!!!")
+else :
+    print("The Winner is Computer!!!")
--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Rock-Papper-Scissor-Game
 This is a Rock-Paper-Scissor Game made using the Python.
+
+# About
+User will input the number assign to the value
+Computer will generate a random choice
+1 point wil be given to the user or Computer who will win or if it's a tie
+After Five rounds one who will ave more points will won the round
